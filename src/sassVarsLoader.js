112ee08a773c893345c2ee0bf7791f0795343a47
Salem Ghoweri <salem.ghoweri@pega.com>
2018-07-25 11:25:35 -0400

    feat: forking to allow ability to watch additional files that aren't pulled in directly

--- src/sassVarsLoader.js
+++ src/sassVarsLoader.js
@@ -1,4 +1,5 @@
 import loaderUtils from 'loader-utils'
+import globby from 'globby'
 import readVarsFromJSONFiles from './utils/readVarsFromJSONFiles'
 import readVarsFromJavascriptFiles from './utils/readVarsFromJavascriptFiles'
 import readSassFiles from './utils/readSassFiles'
@@ -10,9 +11,18 @@ export default function(content) {
 
   const options = loaderUtils.getOptions(this) || {}
   const files = options.files || []
+  const extraWatch = options.extraWatch || []
   const syntax = options.syntax || 'scss'
 
-  watchFilesForChanges(this, files)
+  let mergedFileArray = files
+
+  const uniqueArrayItems = arrArg => arrArg.filter((elem, pos, arr) => arr.indexOf(elem) === pos)
+
+  if (extraWatch.length > 0) {
+    mergedFileArray = mergedFileArray.concat(globby.sync(extraWatch))
+  }
+
+  watchFilesForChanges(this, uniqueArrayItems(mergedFileArray))
 
   const vars = {
     ...readVarsFromJSONFiles(files),
